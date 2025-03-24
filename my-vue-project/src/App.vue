a05b217ec18f847087b57f2692cf5be94fefc158
Pedro Pablo Osorio San Martín <pedro.osorio04@inacapmail.cl>
2025-03-24 13:12:24 -0300

    inicio curso

---
+++ my-vue-project/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from "vue";
+
+let numer = ref(0);
+
+setInterval(() => {
+  numer.value++;
+}, 1);
+
+let name = 'Perro'
+</script>
+
+<template>
+  <h1>Hello {{ numer }}</h1>
+</template>
+
+<style scoped></style>
